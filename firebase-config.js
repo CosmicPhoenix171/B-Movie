7feee06785023418dcb3a4b0b88c46c74714285f
cosmicphoenix171 <fireball0000@hotmail.com>
2025-10-05 23:32:29 -0500

    Add Firebase configuration file for B-Movie Ratings

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Firebase live config for B‑Movie Ratings.
+// These keys are public identifiers (not secrets). If you open source the repo
+// and want to keep config out of version control, add `firebase-config.js` to .gitignore.
+// Toggle remote sync by setting FIREBASE_ENABLED to true/false.
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyABTGhhnWn0UMdlS_HHFwnxuvRxZSGCBJQ",
+  authDomain: "b-movie-ratings.firebaseapp.com",
+  projectId: "b-movie-ratings",
+  storageBucket: "b-movie-ratings.firebasestorage.app",
+  messagingSenderId: "646385733783",
+  appId: "1:646385733783:web:4fd26fa7323a02bab619d2",
+  measurementId: "G-0BST48BQQ6"
+};
+
+window.FIREBASE_ENABLED = true;
